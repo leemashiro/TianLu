8ac5629ccc6b7155990849dffb8ac7cab1e0f561
By丶小桐 <43975634+leemashiro@users.noreply.github.com>
2018-12-18 21:43:13 +0800

    //天路隧道工程demo

--- src/main/java/TianLu_ProductionData/InstrumentID.java
+++ src/main/java/TianLu_ProductionData/InstrumentID.java
@@ -12,32 +12,32 @@ public class InstrumentID {
     }
     //弹性波设备ID   tsp_id
     public static String getTsp_id(){
-        String Tsp_id = "tsp-"+String.valueOf(getNum(1, 20)).substring(1);
+        String Tsp_id = "tsp-"+String.valueOf(getNum(1,200)).substring(1);
         return Tsp_id;
     }
     //检波器ID
     public static String getDetector_id(){
-        String detector_id = "det-"+String.valueOf(getNum(1, 2000)).substring(1);
+        String detector_id = "det-"+String.valueOf(getNum(1,2000)).substring(1);
         return detector_id;
     }
     //激发炮点ID       shotPoint_id
     public static String getShotPoint_id(){
-        String shotPoint_id = "det-"+String.valueOf(getNum(1, 1000)).substring(1);
+        String shotPoint_id = "det-"+String.valueOf(getNum(1,1000)).substring(1);
         return shotPoint_id;
     }
     //高频接收天线ID   receiving_antenna_id
     public static String getReceiving_antenna_id(){
-        String receiving_antenna_id = "ant-"+String.valueOf(getNum(1, 1000)).substring(1);
+        String receiving_antenna_id = "ant-"+String.valueOf(getNum(1,1000)).substring(1);
         return receiving_antenna_id;
     }
     //电极ID    electrode_id
     public static String getElectrode_id(){
-        String getElectrode_id = "ele-"+String.valueOf(getNum(1, 1000)).substring(1);
+        String getElectrode_id = "ele-"+String.valueOf(getNum(1,1000)).substring(1);
         return getElectrode_id;
     }
     //应力传感器ID   Stress_id
     public static String getStress_id(){
-        String getStress_id = "stre-"+String.valueOf(getNum(1, 1000)).substring(1);
+        String getStress_id = "stre-"+String.valueOf(getNum(1,1000)).substring(1);
         return getStress_id;
     }
 
