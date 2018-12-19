b24440aad21893bdb5bd5e0d562d599df71b8b55
By丶小桐 <43975634+leemashiro@users.noreply.github.com>
2018-12-19 14:31:36 +0800

    //更新版本

--- src/main/java/TianLu_ProductionData/DataBag.java
+++ src/main/java/TianLu_ProductionData/DataBag.java
@@ -3,9 +3,11 @@ package TianLu_ProductionData;
 import com.cloudwise.sdg.dic.DicInitializer;
 import com.cloudwise.sdg.template.TemplateAnalyzer;
 
+import static TianLu_ProductionData.TimeFormat.nowTime;
+
 public class DataBag {
     public static AllDataForJson getData() {
-        String nowTime = TimeFormat.nowTime();
+        String nowTime = nowTime();
         //数据词典生成
         try {
             DicInitializer.init();
@@ -21,7 +23,7 @@ public class DataBag {
         String electrode_adr = new TemplateAnalyzer("TianLu","$Dic{coordinate}" ).analyse();
         String s_wave_velocity = new TemplateAnalyzer("TianLu","$Dic{s_wave_velocity}" ).analyse();
         String p_wave_velocity = new TemplateAnalyzer("TianLu", "$Dic{p_wave_velocity}").analyse();
-        AllDataForJson dataJson = new AllDataForJson(nowTime,
+        AllDataForJson dataJson = new AllDataForJson(nowTime(),
                 InstrumentID.getTsp_id(),
                 InstrumentID.getDetector_id(),
                 InstrumentID.getShotPoint_id(),
